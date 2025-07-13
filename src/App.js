ee5b73f2f4571fc2e21b568be97b8296d876665a
Asim Poudel <asim.pdl56@gmail.com>
2025-07-13 16:14:53 +0545

    Added design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [count, setCount] = useState(0);
+	return (
+		<>
+			<div className="container d-flex justify-content-center align-items-center vh-100">
+				<div className="card text-center shadow p-4" style={{width: "350px"}}>
+					<h2 className="mb-4">Counter App</h2>
+
+					<div className="display-4 fw-bold mb-4" id="count">
+						{count}
+					</div>
+
+					<div className="d-grid gap-2 d-md-flex justify-content-md-center">
+						<button
+							className="btn btn-success me-md-2"
+							id="increase"
+							onClick={() => {
+								console.log(count);
+								setCount(count + 1);
+							}}
+						>
+							+ Increase
+						</button>
+						<button
+							className="btn btn-danger me-md-2"
+							id="decrease"
+							onClick={() => {
+								if (count > 0) {
+									setCount(count - 1);
+								}
+							}}
+						>
+							- Decrease
+						</button>
+						<button
+							className="btn btn-secondary"
+							id="reset"
+							onClick={() => {
+								setCount((count = 0));
+							}}
+						>
+							Reset
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
